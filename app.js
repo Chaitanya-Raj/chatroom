d4024ccf3b935c2aaf07185f4e4f285aa6e5397e
Chaitanya Raj <chaitanyarajdixit@gmail.com>
2020-08-19 17:29:48 +0530

    prepare to deploy

--- app.js
+++ app.js
@@ -44,6 +44,22 @@ io.sockets.on("connection", (socket) => {
   });
 });
 
+//Static file declaration
+app.use(express.static(path.join(__dirname, "client/build")));
+
+//production mode
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "client/build")));
+  app.get("*", (req, res) => {
+    res.sendfile(path.join((__dirname = "client/build/index.html")));
+  });
+}
+
+//build mode
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/client/public/index.html"));
+});
+
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
