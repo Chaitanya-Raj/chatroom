c28cd09b8706894d4ca19dd08992d6d85edeca9f
Chaitanya Raj <chaitanyarajdixit@gmail.com>
2020-08-19 17:50:17 +0530

    remove hardcoded path

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import io from "socket.io-client";
 import "./App.css";
 
-const socket = io.connect("http://localhost:5000");
+const socket = io.connect();
 
 function App() {
   const [msg, setMsg] = useState("");
